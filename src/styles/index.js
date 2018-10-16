370d5e255615cbecb5883e1b3247ddc7e5689587
osamu38 <osamu.osamu.0622@gmail.com>
2018-10-17 00:15:52 +0900

    update package.json and add styled-reset

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,4 +1,71 @@
 /* @flow */
+/* eslint-disable no-unused-expressions */
 
-import 'styles/reset';
-import 'styles/base';
+import { injectGlobal } from 'styled-components';
+import reset from 'styled-reset';
+import { colors } from 'styles/variables';
+
+injectGlobal`
+  ${reset}
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+  html {
+    min-height: 100%;
+    text-size-adjust: 100%;
+  }
+  body {
+    position: relative;
+    overflow-x: hidden;
+    min-height: 100%;
+    background-color: ${colors.white};
+    color: ${colors.superDarkGray};
+    word-wrap: break-word;
+    font-size: 14px;
+    font-family: 'Avenir Next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+    line-height: 1.5;
+    -webkit-touch-callout: none;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  img {
+    vertical-align: middle;
+    -webkit-touch-callout: none;
+  }
+  hr {
+    margin: 0;
+  }
+  span,
+  i {
+    display: inline-block;
+    vertical-align: middle;
+  }
+  li {
+    list-style: none;
+  }
+  input,
+  textarea,
+  button {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    border: none;
+    border-radius: 0;
+    background-color: inherit;
+    color: inherit;
+    vertical-align: middle;
+    appearance: none;
+  }
+  select {
+    border-radius: 0;
+  }
+  input:-webkit-autofill,
+  textarea:-webkit-autofill {
+    box-shadow: 0 0 0 1000px white inset;
+  }
+`;
